37b22ec8d5d3a268f00fce278559e85c1db8ae1f
Maciej Koziel <maciejkoziel@Maciejs-MacBook-Pro.local>
2017-12-11 15:47:31 +0100

    Classes added.

---
+++ TestsTest1/Classes.swift
@@ -0,0 +1,25 @@
+//
+//  Classes.swift
+//  TestsTest1
+//
+//  Created by Maciej Koziel on 11/12/2017.
+//  Copyright © 2017 Maciej Koziel. All rights reserved.
+//
+
+import Foundation
+
+class A: P {
+    
+}
+
+class B: A {
+    
+}
+
+protocol P {
+    
+}
+
+struct S {
+    
+}
