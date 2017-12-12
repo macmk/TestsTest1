0f95055486e35e139c0083260fe051fb0f9ce3d6
Maciej Koziel <maciejkoziel@Maciejs-MacBook-Pro.local>
2017-12-12 15:43:31 +0100

    Class references 2.

--- TestsTest1/Classes.swift
+++ TestsTest1/Classes.swift
@@ -9,7 +9,13 @@
 import Foundation
 
 class A: P {
+    let z = C()
+    let z2: E = E()
     
+    func funcD() {
+        let x = B()
+        let y: A = A()
+    }
 }
 
 class B: A {
@@ -21,13 +27,7 @@ class C: B {
 }
 
 class D: A {
-    let z = C()
-    let z2: E = E()
     
-    func funcD() {
-        let x = B()
-        let y: A = A()
-    }
 }
 
 class E {
