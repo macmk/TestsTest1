28334ef87f2b170e7b0d9d945da1232a1200b286
Maciej Koziel <maciejkoziel@Maciejs-MacBook-Pro.local>
2017-12-14 13:55:50 +0100

    Metrics tests.

--- TestsTest1/Classes.swift
+++ TestsTest1/Classes.swift
@@ -9,26 +9,29 @@
 import Foundation
 
 class A: P {
-    let z = C()
+    let z = A2()
     let z2: E = E()
     
     func funcD() {
-        let x = B()
+        let a3 = A3()
         let y: A = A()
+        let a4 = A4()
+        let a5 = A5()
+        let a6 = A6()
     }
 }
 
-class B: A {
-    
-}
+class A2: A {}
+class A21: A {}
+class A22: A {}
+class A23: A {}
+class A24: A {}
+class A25: A {}
 
-class C: B {
-    
-}
-
-class D: A {
-    
-}
+class A3: A2 {}
+class A4: A3 {}
+class A5: A4 {}
+class A6: A5 {}
 
 class E {
     func funcE(a: A) {
